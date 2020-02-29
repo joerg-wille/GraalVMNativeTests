e05aa93f557d02056ca73a0ef8a2376fbc7145db
joerg <joerg.wille@gmail.com>
2020-02-29 01:52:03 +0100

    native-async: enable https follow redirects

--- swagger-native-async/src/main/java/net/jbw/App.java
+++ swagger-native-async/src/main/java/net/jbw/App.java
@@ -3,6 +3,7 @@ package net.jbw;
 import java.net.Authenticator;
 import java.net.PasswordAuthentication;
 import java.net.http.HttpClient;
+import java.net.http.HttpClient.Redirect;
 import java.time.Duration;
 
 import net.jbw.openproject.client.api.UsersApi;
@@ -18,17 +19,17 @@ public class App {
 //	// branch stable/10.3.1 joerg@kermit
 //	private static final String APIKEY = "3275030fced07dedcf3fb1d609e833d1788d29bbfb167c1a17ec93c5af118cf4";
 
-	private static final String SERVER_SCHEME = "https";
-	private static final String SERVER_HOST = "joergwille.openproject.com";
-	private static final String SERVER_BASE_PATH = "/";
-	// https://joergwille.openproject.com
-	private static final String APIKEY = "5b8a073a2e97bdd8a54bce05e1cac36cc83d8160706e40929088687c598383cf";
-
 //	private static final String SERVER_SCHEME = "https";
-//	private static final String SERVER_HOST = "make.haus";
+//	private static final String SERVER_HOST = "joergwille.openproject.com";
 //	private static final String SERVER_BASE_PATH = "/";
-//	// branch release/10.4 joerg@indspdev
-//	private static final String APIKEY = "34967d2b46496e94bdb1da235e470c4fb6e2f9f4e7e1ed0e8660dce93239722a";
+//	// https://joergwille.openproject.com
+//	private static final String APIKEY = "5b8a073a2e97bdd8a54bce05e1cac36cc83d8160706e40929088687c598383cf";
+
+	private static final String SERVER_SCHEME = "https";
+	private static final String SERVER_HOST = "make.haus";
+	private static final String SERVER_BASE_PATH = "/";
+	// branch release/10.4 joerg@indspdev
+	private static final String APIKEY = "34967d2b46496e94bdb1da235e470c4fb6e2f9f4e7e1ed0e8660dce93239722a";
 
 	private static final String USERNAME = "apikey";
 
@@ -36,14 +37,17 @@ public class App {
 		System.out.println("Hello swagger-native-async!");
 		System.out.println("Requires Java 11!");
 		System.out.println("Detected Java Version: " + System.getProperty("java.version"));
+		System.out.println("Server: " + SERVER_SCHEME + "://" + SERVER_HOST + SERVER_BASE_PATH);
 
-		ApiClient defaultClient = Configuration.getDefaultApiClient().setHttpClientBuilder(
-				HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).authenticator(new Authenticator() {
-					@Override
-					protected PasswordAuthentication getPasswordAuthentication() {
-						return new PasswordAuthentication(USERNAME, APIKEY.toCharArray());
-					}
-				})).setReadTimeout(Duration.ofSeconds(10)).setScheme(SERVER_SCHEME).setHost(SERVER_HOST)
+		ApiClient defaultClient = Configuration.getDefaultApiClient()
+				.setHttpClientBuilder(HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1)
+						.followRedirects(Redirect.NORMAL).authenticator(new Authenticator() {
+							@Override
+							protected PasswordAuthentication getPasswordAuthentication() {
+								return new PasswordAuthentication(USERNAME, APIKEY.toCharArray());
+							}
+						}))
+				.setReadTimeout(Duration.ofSeconds(10)).setScheme(SERVER_SCHEME).setHost(SERVER_HOST)
 				.setBasePath(SERVER_BASE_PATH);
 
 		final UsersApi usersApi = new UsersApi(defaultClient);
